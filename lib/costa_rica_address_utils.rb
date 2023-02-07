336d561f7d3cea4e95617dd05418d681fa0f38a6
vicvans20 <vicvans20@gmail.com>
2023-02-07 10:09:29 -0600

    First commit

---
+++ lib/costa_rica_address_utils.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "costa_rica_address_utils/version"
+
+module CostaRicaAddressUtils
+  class Error < StandardError; end
+  # Your code goes here...
+end
